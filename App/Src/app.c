7a1c552ece50c6f128fbc9c077c41c8367e16ab1
kramxkomitus <kramxkomitus@gmail.com>
2023-03-01 22:12:56 +0400

    fix OpenOCD settings

--- App/Src/app.c
+++ App/Src/app.c
@@ -2,6 +2,9 @@
 
 void app()
 {
-    HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-    HAL_Delay(100);
+    while (1)
+    {
+        HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
+        HAL_Delay(100);
+    }
 }
